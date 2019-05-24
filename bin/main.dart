c81c240e6648ad49d22fd639c5698f9056f3c6b7
Buerkut <Buerkut@foxmail.com>
2019-05-24 18:20:56 +0800

    add hash table

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,9 @@
-import 'package:data_struct/recursion/permute.dart';
 // import 'dart:math';
+import 'package:data_struct/hash_table/hash_table.dart';
 
 void main() {
-  var s = 'zengzenga';
-  for (var i = 0; i < s.length; i++) {
-    print(s[i].codeUnits);
-  }
+  var a = List.generate(10, (i) => '$i'), b = List.generate(10, (i) => i << 1);
+  var table = HashTable(6);
+  for (var i = 0; i < a.length; i++) table.insert(a[i], b[i]);
+  print(table);
 }
--- lib/hash_table/hash_table.dart
+++ lib/hash_table/hash_table.dart
@@ -1,30 +1,100 @@
-class TableEntry<K, V> {
-  K _key;
-  V _value;
-  TableEntry(this._key, this._value);
-
-  K get key => _key;
-  V get value => _value;
-}
-
 class HashTable<K, V> {
-  List<TableEntry<K, V>> _table;
+  List<_TableEntry<K, V>> _table;
   int _itemCount;
 
-  HashTable()
-      : _table = []..length = 64,
-        _itemCount = 0;
+  factory HashTable.fromIterables(Iterable<K> keys, Iterable<V> values) {
+    if (keys.length != values.length)
+      throw StateError('number of keys does not equal to the number of values');
+    var htb = HashTable<K, V>(keys.length << 1);
+    for (var i = 0; i < keys.length; i++)
+      htb.insert(keys.elementAt(i), values.elementAt(i));
+    return htb;
+  }
 
-  int _hashCode(K key) {
+  HashTable([initalSize])
+      : _itemCount = 0,
+        _table = List(initalSize ?? 256);
+
+  int get size => _itemCount;
+  bool get isEmpty => size == 0;
+
+  bool contains(K key) => _table[_address(key)] != null;
+
+  void clear() {
+    _table = null;
+    _itemCount = 0;
+  }
+
+  void insert(K key, V value) {
+    if (key == null) throw StateError('key and value can not be null!');
+    _insert(_TableEntry(key, value));
+    if (_itemCount++ == _table.length - 1) _dilate();
+  }
+
+  V remove(K key) {
+    var i = _address(key), entry = _table[i];
+    if (entry != null) {
+      _table[i] = null;
+      _itemCount--;
+    }
+    return entry?.value;
+  }
+
+  V operator [](K key) => _table[_address(key)]?.value;
+
+  void operator []=(K key, V value) => insert(key, value);
+
+  String toString() => _table.toString();
+
+  void _insert(_TableEntry<K, V> entry) => _table[_address(entry.key)] = entry;
+
+  int _address(K key) {
+    var hashValue = _hash(key, _table.length);
+    while (_table[hashValue] != null && _table[hashValue].key != key)
+      hashValue = _rehash(hashValue, _table.length);
+    return hashValue;
+  }
+
+  void _dilate() {
+    var oldTable = _table;
+    _table = List(oldTable.length << 1);
+    for (var entry in oldTable) _insert(entry);
+  }
+
+  static int _hash<K>(K key, int modelSize) {
     int result;
     if (key is int) {
       result = key;
     } else if (key is double) {
       result = key.floor();
-    } else if (key is String) {
+    } else {
       result = 17;
-      for (var i = 0; i < key.length; i++)
-        result = 37 * result + key[i].codeUnitAt(0) - 96;
-    } else {}
+      var s = key is String ? key : key.toString();
+      for (var i = 0; i < s.length; i++)
+        result = 37 * result + s[i].codeUnitAt(0);
+    }
+    return result % modelSize;
   }
+
+  static int _rehash(int hashValue, int modelSize) {
+    // linear probing;
+    // hashValue++;
+
+    // quadratic probing;
+    // var i = 1;
+    // hashValue += i * i;
+    // i++;
+
+    // double hashing;
+    hashValue += 7 - hashValue % 7;
+    return hashValue % modelSize;
+  }
+}
+
+class _TableEntry<K, V> {
+  K key;
+  V value;
+  _TableEntry(this.key, this.value);
+
+  String toString() => '{$key: $value}';
 }
