3af8593f5661137517511b0175a9f8b647529d15
Buerkut <Buerkut@foxmail.com>
2022-11-11 22:04:25 +0800

    add union find

--- bin/main.dart
+++ bin/main.dart
@@ -2,5 +2,5 @@ import 'package:data_struct/union_find/src/union_find.dart';
 
 void main(List<String> args) {
   var uf = UnionFind(5);
-  print(uf.groupCount);
+  // print(uf._groupCount);
 }
--- lib/union_find/src/union_find.dart
+++ lib/union_find/src/union_find.dart
@@ -1,8 +1,64 @@
 class UnionFind {
-  List _eleAndGroup;
+  List<int> _eleAndGroup;
+  int _groupCount;
 
-  final int groupCount;
+  UnionFind(this._groupCount)
+      : _eleAndGroup = List.generate(_groupCount, (i) => i);
 
-  UnionFind(this.groupCount)
-      : _eleAndGroup = List.generate(groupCount, (i) => i);
+  int get groupCount => _groupCount;
+
+  int find(int p) => _eleAndGroup[p];
+
+  bool connected(int p, int q) => _eleAndGroup[p] == _eleAndGroup[q];
+
+  void union(int p, int q) {
+    if (connected(p, q)) return;
+
+    var pg = find(p);
+    var qg = find(q);
+    for (var i = 0; i < _eleAndGroup.length; i++) {
+      if (_eleAndGroup[i] == pg) _eleAndGroup[i] = qg;
+    }
+    _groupCount--;
+  }
+}
+
+class UnionFindTree {
+  List<int> _eleAndGroup;
+  int _groupCount;
+  List<int> _sz;
+
+  UnionFindTree(this._groupCount)
+      : _eleAndGroup = List.generate(_groupCount, (i) => i),
+        _sz = List.generate(_groupCount, (_) => 1);
+
+  int get groupCount => _groupCount;
+
+  bool connected(int p, int q) => find(p) == find(q);
+
+  int find(int p) {
+    while (true) {
+      if (p == _eleAndGroup[p]) {
+        return p;
+      }
+      p = _eleAndGroup[p];
+    }
+  }
+
+  void union(int p, int q) {
+    var pRoot = find(p);
+    var qRoot = find(q);
+    if (pRoot == qRoot) return;
+
+    if (_sz[pRoot] < _sz[qRoot]) {
+      _eleAndGroup[pRoot] = qRoot;
+      _sz[qRoot] += _sz[pRoot];
+    } else {
+      _eleAndGroup[qRoot] = pRoot;
+      _sz[pRoot] += _sz[qRoot];
+    }
+
+    _eleAndGroup[pRoot] = qRoot;
+    _groupCount--;
+  }
 }
