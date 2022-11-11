1c80b1675991e29d7f5c7d05516eb63c3b39135f
Buerkut <Buerkut@foxmail.com>
2022-11-11 22:09:01 +0800

    add union find

--- bin/main.dart
+++ bin/main.dart
@@ -1,6 +1,6 @@
-import 'package:data_struct/union_find/src/union_find.dart';
+// import 'package:data_struct/union_find/src/union_find.dart';
 
 void main(List<String> args) {
-  var uf = UnionFind(5);
+  // var uf = UnionFind(5);
   // print(uf._groupCount);
 }
